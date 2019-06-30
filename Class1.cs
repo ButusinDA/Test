7f4d6bba9c7cb8b5c3da8b2cadc560947ae8cba6
dshb1 <dshb1@DESKTOP-80VVO84>
2019-06-30 11:38:21 +0300

    Console.WriteLine

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Class1
+    {
+        static void Main ()
+        {
+            decimal d = 213434.234m;
+            Console.WriteLine("Money = {0:c}", d);
+            Console.ReadKey();
+        }
+    }
+}
